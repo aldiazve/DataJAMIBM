ef36655a36a75a12bb677de24770ac52d0d6660a
Alejandro Díaz Vecchio <aldiazve@gmail.com>
2019-08-31 12:17:20 -0500

    added map to app component

--- save-team-app/src/App.js
+++ save-team-app/src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Map = styled("div")`
+  width: 500px;
+  heigth: 500px;
+`;
 
 //The main purpose of this class is to identify if there is a logged user and redirect him to the proper view.
 class SafeTeamApp extends React.Component {
   constructor(props) {
     super(props);
+    var map = window.map;
     this.state = {
+      map,
     };
   };
 
