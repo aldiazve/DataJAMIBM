0af1604dc3f71dc73ef47f9fea82f9932092d830
Alejandro Díaz Vecchio <aldiazve@gmail.com>
2019-08-31 11:34:34 -0500

    init

---
+++ save-team-app/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+/*
+var map;
+const onGoogleMapResponse = () => {
+  map = new google.maps.Map(document.getElementById('googleMapContainer'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8
+  });
+}
+*/
+//The main purpose of this class is to identify if there is a logged user and redirect him to the proper view.
+class SafeTeamApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  };
+
+  componentDidMount(){
+  };
+
+  render() {
+    return (
+      <div className='App'>
+      </div>
+      );
+    }
+}
+
+export default IronHacksApp;
